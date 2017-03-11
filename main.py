d1c33e9bd5fbef0893747cd10030a8c9d84cf649
vadik likholetov <vadikas@gmail.com>
2017-03-12 02:03:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,133 @@
+from flask import Flask
+from flask import request, redirect, session, render_template
+import hashlib
+import urllib
+import json
+import redis
+
+app = Flask(__name__)
+app.secret_key = "#d\xe9X\x00\xbe~Uq\x1fs\t\xb4\x99\xa3\x87\xe6.\xd1_\xebX\xae\x81'"
+r = redis.StrictRedis()
+ttl = 60
+ntries = 5
+
+
+#
+
+
+def calc_sha256(block):
+    sha256 = hashlib.sha256()
+    sha256.update(block.encode("utf-8"))
+    return sha256.hexdigest()
+
+
+def mokum_auth(apikey):
+    try:
+        req = urllib.request.Request("https://mokum.place/api/v1/whoami.json")
+        req.add_header('Content-Type', 'application/json')
+        req.add_header('Accept', 'application/json')
+        req.add_header('X-API-Token', apikey)
+        resp = urllib.request.urlopen(req)
+        message = json.loads(resp.read().decode("utf8"))
+        if message["user"]["name"]:
+            return message["user"]["name"]
+    except:
+        return False
+
+
+def crash_num(login):
+    try:
+        return len(r.lrange(login, 0, -1))
+    except:
+        return 0
+
+
+def crash_check(login, crash):
+    for i in r.lrange(login, 0, -1):
+
+        if str(calc_sha256(crash)) == i.decode("utf-8"):
+            r.lrem(login, 0, i)
+            return True
+    return False
+
+
+def crash_add(login, crash):
+    r.lpush(crash, calc_sha256(login))
+    return True
+
+
+def crash_tries(login, incr=0):
+    try:
+        tries = int(r.get(login + "+count"))
+    except:
+        tries = ntries
+        r.setex(login + "+count", ttl, tries)
+
+    if incr > 0:
+        if tries > 0:
+            tries -= 1
+            r.setex(login + "+count", ttl, tries)
+
+    return tries
+
+
+@app.route('/')
+def process():
+    if 'login' in session:
+        login = session['login']
+        return render_template('main.html', login=login, num=crash_num(login))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/guessortry', methods=['POST'])
+def makeguess():
+    guess = fail = ""
+
+    if 'login' in session:
+        login = session['login']
+        crash = request.form['crash']
+        crash = crash.strip()
+        if len(crash) > 0:
+
+            if crash_tries(login, 1) > 0:
+                tries = "Yon have " + str(crash_tries(login)) + "tries left."
+
+                if crash_check(login, crash):
+                    guess = "Yooohoo! You've guessed! It's " + crash + "!"
+                else:
+                    fail = "Not this time, but we'll let " + crash + " know about your passion."
+                    crash_add(login, crash)
+            else:
+                tries = "You have no tries left :(, try again in a week..."
+        else:
+            if crash_tries(login) > 0:
+                tries = "Yon have " + str(crash_tries(login)) + "tries left."
+            else:
+                tries = "You have no tries left :(, try again in a week..."
+
+        return render_template("main.html", login=login, num=crash_num(login), guess=guess, fail=fail, tries=tries)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    apikey = request.form['apikey']
+
+    login = mokum_auth(apikey)
+    if login:
+        session['login'] = login
+
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    if 'login' in session:
+        del session['login']
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
